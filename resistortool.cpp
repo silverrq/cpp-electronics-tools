f6faf28ba228d4785a1141210058e1d1866512af
silverr217 <sercantsn217@gmail.com>
2025-10-06 00:25:10 +0300

    Add typewriter effect and UTF-8 Turkish character support

--- resistortool.cpp
+++ resistortool.cpp
@@ -1,11 +1,42 @@
-#include <iostream>
+#include <iostream> // Giriş/Çıkış işlemleri için
+#include <string> // string sınıfı için
+#include <chrono> // Zaman işlemleri için
+#include <thread> // Uyku işlemi için
 
-using namespace std;
+// Windows için konsol kodlama ayarları. Linux/MacOS'ta gerek yok. Türkçe karakterler için.
+#ifdef _WIN32
+    #include <windows.h>
+    #include <io.h>
+    #include <fcntl.h>
+#endif
+
+using namespace std; // std ad alanını kullan
+
+
+// Konsolun Türkçe karakterleri doğru görüntülemesi için ayar yapan fonksiyon
+
+void turkce() {
+    #ifdef _WIN32
+        SetConsoleOutputCP(65001); // UTF-8 output
+        SetConsoleCP(65001);       // UTF-8 input
+    #endif
+}
+
+
+// Metin karakter karakter yazdıran ve her karakter arasında gecikme ekleyen fonksiyon
+void YavasYaz(const string& text, int delay_ms = 25)  {
+        for (char karakter: text) {
+            cout << karakter << flush; // Karakteri yazdır
+            this_thread::sleep_for(chrono::milliseconds(delay_ms)); // Gecikme
+        }
+    }
 
 int main() {
+
+    turkce();
     
-    cout << "Hello, World!" << endl;
-    
-    
+    //Başlangıç mesajı
+    YavasYaz("Resistor Calculator'a Hoşgeldiniz!");
+
     return 0;
 }
